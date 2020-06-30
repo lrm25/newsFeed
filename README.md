681553a8602810dbc137f218f45b822876325078
Luke McCrone <luke.r.mccrone@gmail.com>
2020-06-29 23:13:52 -0600

    initial request/response

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # newsFeed
+
+## Virtual Environment
+
+```
+python3 -m venv env
+source env/bin/activate
+```
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import urllib.request
+
 def main():
     print("news feed")
+    with urllib.request.urlopen('http://public.nrao.edu/news') as response:
+        data = response.read()
+        print(data)
 
 if __name__ == "__main__":
     main()
