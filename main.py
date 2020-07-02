a2a7b052ef34844a0a45f2552d46ab198520b5c4
Luke McCrone <luke.r.mccrone@gmail.com>
2020-07-01 22:47:20 -0600

    news subsection

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import urllib.request
 
+from bs4 import BeautifulSoup
+
 def main():
     print("news feed")
     with urllib.request.urlopen('http://public.nrao.edu/news') as response:
         data = response.read()
-        print(data)
+        data_soup = BeautifulSoup(data, 'html.parser')
+        news_section = data_soup.find_all("div", class_="all-news")
+        print(news_section)
 
 if __name__ == "__main__":
     main()
