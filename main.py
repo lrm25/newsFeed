0bdaf63e4acde0cd64f7c8db5e02ac00c08b7d31
Luke McCrone <luke.r.mccrone@gmail.com>
2020-07-02 23:15:39 -0600

    Article titles

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ def main():
     with urllib.request.urlopen('http://public.nrao.edu/news') as response:
         data = response.read()
         data_soup = BeautifulSoup(data, 'html.parser')
-        news_section = data_soup.find_all("div", class_="all-news")
-        print(news_section)
+        news_titles = data_soup.find_all("div", class_="news-list-title")
+        for news_title in news_titles:
+            print(news_title.text)
 
 if __name__ == "__main__":
     main()
