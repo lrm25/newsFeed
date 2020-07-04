f918a3fe337ef0c29271797552c2c96086f36e84
Luke McCrone <luke.r.mccrone@gmail.com>
2020-07-04 15:31:32 -0600

    added date and time

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ import urllib.request
 from bs4 import BeautifulSoup
 
 def main():
-    print("news feed")
     with urllib.request.urlopen('http://public.nrao.edu/news') as response:
         data = response.read()
         data_soup = BeautifulSoup(data, 'html.parser')
-        news_titles = data_soup.find_all("div", class_="news-list-title")
-        for news_title in news_titles:
-            print(news_title.text)
+        all_news = data_soup.find("div", class_="all-news")
+        news_rows = all_news.find_all("div", class_="row")
+        for news_row in news_rows:
+            meta = news_row.find("div", class_="news-list-meta")
+            date_and_time = meta.text.split('|')
+            print(date_and_time[0])
+            headline = news_row.find("div", class_="news-list-title")
+            print(headline.text)
 
 if __name__ == "__main__":
     main()
