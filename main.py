436b8f2cfa2aa3521d2eb8cbbb1dc2e61f51bb45
Luke McCrone <luke.r.mccrone@gmail.com>
2020-07-10 01:02:13 -0600

    more error handling

--- main.py
+++ main.py
@@ -2,25 +2,31 @@ import urllib.request
 
 from bs4 import BeautifulSoup
 
+nrao_url = 'http://public.nrao.edu/news'
+
+def response_read_func(response):
+    return response.read()
+
 def parse_nrao(url):
-    try:
-        response = urllib.request.urlopen(url)
-        data = response.read()
-        data_soup = BeautifulSoup(data, 'html.parser')
-        all_news = data_soup.find("div", class_="all-news")
-        news_rows = all_news.find_all("div", class_="row")
-        for news_row in news_rows:
-            meta = news_row.find("div", class_="news-list-meta")
-            date_and_time = meta.text.split('|')
-            print(date_and_time[0])
-            headline = news_row.find("div", class_="news-list-title")
-            print(headline.text)
-    except ValueError as v:
-        print("ValueError: " + str(v))
+    response = urllib.request.urlopen(url)
+    data = response_read_func(response)
+    if data == None:
+        raise ValueError("No data returned")
+    data_soup = BeautifulSoup(data, 'html.parser')
+    all_news = data_soup.find("div", class_="all-news")
+    news_rows = all_news.find_all("div", class_="row")
+    for news_row in news_rows:
+        meta = news_row.find("div", class_="news-list-meta")
+        date_and_time = meta.text.split('|')
+        print(date_and_time[0])
+        headline = news_row.find("div", class_="news-list-title")
+        print(headline.text)
 
 def main():
-    parse_nrao('http://public.nrao.edu/news')
-        
+    try:
+        parse_nrao(nrao_url)
+    except ValueError as v:
+        print("ValueError: " + str(v))
 
     with urllib.request.urlopen('https://www.eso.org/public/news') as response:
         data = response.read()
--- test_main.py
+++ test_main.py
@@ -1,4 +1,16 @@
 import main
+import pytest
 
-def test_parse_nrao():
-    main.parse_nrao("")
+def test_parse_nrao_bad_url():
+    with pytest.raises(ValueError):
+        assert main.parse_nrao("")
+
+def mock_response_read_func(response):
+    return None
+
+def test_parse_nrao_no_data():
+    with pytest.raises(ValueError):
+        orig_func = main.response_read_func
+        main.response_read_func = mock_response_read_func
+        main.parse_nrao(main.nrao_url)
+        main.response_read_func = orig_func
